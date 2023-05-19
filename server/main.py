cb03eebb92266eef5d16f8f87080377ff00762a0
rifatrakib <Rakib.1508@outlook.com>
2023-05-19 15:38:00 +0600

    create database and tables if needed on startup

--- server/main.py
+++ server/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 
 from server.config.factory import settings
+from server.models.managers import create_db_and_tables
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    print("Starting up...")
+    create_db_and_tables()
+    print("Startup complete!")
 
 
 @app.get("/health")
