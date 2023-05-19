784c6bc31ed35776bcfdc7f5c8a4501ed2111b06
rifatrakib <Rakib.1508@outlook.com>
2023-05-19 16:15:08 +0600

    add postgreSQL specific config variables

--- server/config/environments/base.py
+++ server/config/environments/base.py
@@ -1,4 +1,6 @@
-from pydantic import BaseSettings, Extra
+from typing import Union
+
+from pydantic import BaseSettings, Extra, Field
 
 
 class RootConfig(BaseSettings):
@@ -10,6 +12,13 @@ class RootConfig(BaseSettings):
 
 class BaseConfig(RootConfig):
     APP_NAME: str
+
+    # SQL Database Configurations
+    RDS_HOST: Union[str, None] = Field(default=None)
+    RDS_PORT: Union[str, None] = Field(default=None)
+    RDS_USER: Union[str, None] = Field(default=None)
+    RDS_PASS: Union[str, None] = Field(default=None)
+    RDS_NAME: Union[str, None] = Field(default=None)
 
     class Config:
         env_file = "configurations/.env"
