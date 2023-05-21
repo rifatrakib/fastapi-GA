5b8b10ae38fe6bc393201d60a65a0760b8363af7
rifatrakib <Rakib.1508@outlook.com>
2023-05-21 12:46:12 +0600

    create authentication token on successful register

--- server/database/users/auth.py
+++ server/database/users/auth.py
@@ -2,11 +2,11 @@ from sqlmodel import Session, create_engine
 
 from server.config.factory import settings
 from server.models.schemas.users import UserAccount
-from server.schemas.inc.auth import SignupSchema
+from server.schemas.inc.auth import SignupRequestSchema
 from server.security.authentication import pwd_context
 
 
-def create_user_account(payload: SignupSchema) -> UserAccount:
+def create_user_account(payload: SignupRequestSchema) -> UserAccount:
     engine = create_engine(settings.RDS_URI, echo=True)
     hashed_password = pwd_context.hash_plain_password(payload.password)
     user = UserAccount(
--- server/routes/auth.py
+++ server/routes/auth.py
@@ -1,13 +1,22 @@
 from fastapi import APIRouter, status
 
 from server.database.users.auth import create_user_account
-from server.schemas.inc.auth import SignupSchema
+from server.schemas.inc.auth import SignupRequestSchema
+from server.schemas.out.auth import SignupResponseSchema
+from server.security.token import create_jwt
 from server.utils.enums import Tags
 
 router = APIRouter(prefix="/auth", tags=[Tags.authentication])
 
 
-@router.post("/signup", status_code=status.HTTP_201_CREATED)
-async def register(payload: SignupSchema):
+@router.post(
+    "/signup",
+    summary="Register new user",
+    description="Register a new user account.",
+    response_model=SignupResponseSchema,
+    status_code=status.HTTP_201_CREATED,
+)
+async def register(payload: SignupRequestSchema):
     new_user = create_user_account(payload)
-    return new_user.dict()
+    token = create_jwt(new_user)
+    return {"access_token": token, "token_type": "Bearer"}
