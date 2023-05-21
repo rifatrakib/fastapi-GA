ef89c60673cb0022740bda6c3be35473c6670792
rifatrakib <Rakib.1508@outlook.com>
2023-05-21 06:56:24 +0600

    add separate base schemas for request and response

--- server/schemas/base.py
+++ server/schemas/base.py
@@ -1,6 +1,6 @@
-from typing import Any
+from typing import Any, Union
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Extra, validator
 
 from server.utils.formatters import format_dict_key_to_camel_case
 
@@ -14,3 +14,16 @@ class BaseAPISchema(BaseTokenAPISchema):
     class Config:
         orm_mode: bool = True
         alias_generator: Any = format_dict_key_to_camel_case
+
+
+class BaseRequestSchema(BaseAPISchema):
+    class Config:
+        extra = Extra.forbid
+
+
+class BaseResponseSchema(BaseAPISchema):
+    id: Union[str, None] = None
+
+    @validator("id", pre=True)
+    def convert_id(cls, v):
+        return str(v) if v else None
