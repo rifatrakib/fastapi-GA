eb6e8066a3c4fdf1b82cf24230b0585872b043ad
rifatrakib <Rakib.1508@outlook.com>
2023-05-24 07:19:00 +0600

    add cli commands to manage containers

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ from typer import Typer
 app = Typer()
 
 
-@app.command(name="runserver")
+@app.command(name="run-server")
 def run_api_server(mode: str = "development"):
     os.environ["MODE"] = mode
 
@@ -15,6 +15,21 @@ def run_api_server(mode: str = "development"):
         subprocess.Popen("wsl redis-server", shell=True)
 
     uvicorn.run("server.main:app", host="0.0.0.0", port=8000, reload=True)
+
+
+@app.command(name="run-containers")
+def run_containers(show_logs: bool = True):
+    command = "docker compose up"
+
+    if not show_logs:
+        command = f"{command} -d"
+
+    subprocess.run(command, shell=True)
+
+
+@app.command(name="stop-containers")
+def stop_containers():
+    subprocess.run("docker compose down", shell=True)
 
 
 if __name__ == "__main__":
