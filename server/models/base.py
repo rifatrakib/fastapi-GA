ca37a508b9b27e0a409e859355b9313a27dc0034
rifatrakib <Rakib.1508@outlook.com>
2023-05-25 09:05:05 +0600

    generalize timestamp fields, update db table schema

--- server/models/base.py
+++ server/models/base.py
@@ -10,6 +10,9 @@ from server.utils.formatters import format_datetime_into_isoformat
 
 
 class BaseModelConfig(BaseModel):
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    last_updated_at: Union[datetime, None] = Field(default=None)
+
     class Config:
         use_enum_values = True
         validate_assignment = True
@@ -20,11 +23,12 @@ class BaseModelConfig(BaseModel):
 class BaseSQLTable(SQLModel, BaseModelConfig):
     id: int = Field(index=True, primary_key=True)
 
+    def update(self):
+        self.last_updated_at = datetime.utcnow()
+        return super().update()
+
 
 class BaseDocumentModel(Document, BaseModelConfig):
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-    last_updated_at: Union[datetime, None] = Field(default=None)
-
     async def save(self):
         if self.id:
             self.last_updated_at = datetime.utcnow()
