8c15fd6eb99867dd2bbd239e5778af51ed4af7a4
rifatrakib <Rakib.1508@outlook.com>
2023-08-06 19:32:27 +0600

    added new fields to product document schema

--- server/models/documents/products.py
+++ server/models/documents/products.py
@@ -1,3 +1,5 @@
+from typing import List, Union
+
 from pydantic import Field
 
 from server.models.base import BaseDocumentModel
@@ -5,8 +7,13 @@ from server.models.base import BaseDocumentModel
 
 class ProductDocument(BaseDocumentModel):
     name: str = Field(..., title="Product name")
+    brand: str = Field(..., title="Product brand")
+    model: str = Field(..., title="Product model")
     description: str = Field(..., title="Product description")
     price: float = Field(..., title="Product price")
+    available_sizes: List[str] = Field(default_factory=list, title="Available sizes of the product")
+    colors: List[str] = Field(default_factory=list, title="Available colors of the product")
+    rating: Union[float, None] = Field(default=None, title="Average product rating")
 
     class Settings:
         name = "products"
