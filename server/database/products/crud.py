8efdd0ae2d2fba241422b34a8393b31094e9867a
rifatrakib <Rakib.1508@outlook.com>
2023-08-07 20:13:15 +0600

    modify single product read database function

--- server/database/products/crud.py
+++ server/database/products/crud.py
@@ -1,5 +1,11 @@
+from beanie import PydanticObjectId
+
 from server.models.documents.products import ProductDocument
+from server.utils.messages import raise_404_not_found
 
 
 async def read_product_by_id(product_id: str) -> ProductDocument:
-    return await ProductDocument.get(product_id)
+    product = await ProductDocument.get(PydanticObjectId(product_id))
+    if not product:
+        raise raise_404_not_found("Product not found")
+    return product
