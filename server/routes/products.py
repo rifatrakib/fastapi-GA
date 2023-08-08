e8c37a60781b7a51d5289531c0a4cb834ea8c8f9
rifatrakib <Rakib.1508@outlook.com>
2023-08-08 12:46:57 +0600

    integrate products router with the main app

--- server/routes/products.py
+++ server/routes/products.py
@@ -1,11 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from server.database.products.crud import read_product_by_id
+from server.schemas.out.products import ProductResponse
 from server.utils.enums import Tags
 
-router = APIRouter(prefix="/products", tags=Tags.products)
+router = APIRouter(prefix="/products", tags=[Tags.products])
 
 
-@router.get("/{product_id}")
+@router.get("/{product_id}", response_model=ProductResponse)
 async def read_single_product(product_id: str):
-    return await read_product_by_id(product_id)
+    try:
+        return await read_product_by_id(product_id)
+    except HTTPException as e:
+        raise e
