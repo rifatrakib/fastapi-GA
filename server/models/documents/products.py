0cad018ac8d162e0255ffe8d6069bd75926b4418
rifatrakib <Rakib.1508@outlook.com>
2023-08-09 21:26:41 +0600

    refactor the product document model

--- server/models/documents/products.py
+++ server/models/documents/products.py
@@ -1,20 +1,8 @@
-from typing import List, Union
-
-from pydantic import Field
-
 from server.models.base import BaseDocumentModel
+from server.schemas.common.products import ProductBase
 
 
-class ProductDocument(BaseDocumentModel):
-    name: str = Field(..., title="Product name")
-    brand: str = Field(..., title="Product brand")
-    model: str = Field(..., title="Product model")
-    description: str = Field(..., title="Product description")
-    price: float = Field(..., title="Product price")
-    available_sizes: List[str] = Field(default_factory=list, title="Available sizes of the product")
-    colors: List[str] = Field(default_factory=list, title="Available colors of the product")
-    rating: Union[float, None] = Field(default=None, title="Average product rating")
-
+class ProductDocument(BaseDocumentModel, ProductBase):
     class Settings:
         name = "products"
         indexes = ["name"]
