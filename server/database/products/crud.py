faa25ad8f34eb6783bd92fa2713d88a14990be80
rifatrakib <Rakib.1508@outlook.com>
2023-08-10 13:05:23 +0600

    added new route to create product

--- server/database/products/crud.py
+++ server/database/products/crud.py
@@ -1,7 +1,13 @@
 from beanie import PydanticObjectId
 
 from server.models.documents.products import ProductDocument
+from server.schemas.inc.products import ProductRequest
 from server.utils.messages import raise_404_not_found
+
+
+async def create_product(product: ProductRequest) -> ProductDocument:
+    product = await ProductDocument.insert_one(ProductDocument(**product.dict()))
+    return product
 
 
 async def read_product_by_id(product_id: str) -> ProductDocument:
--- server/routes/products.py
+++ server/routes/products.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 
-from server.database.products.crud import read_product_by_id
+from server.database.products.crud import create_product, read_product_by_id
+from server.schemas.inc.products import ProductRequest
 from server.schemas.out.products import ProductResponse
 from server.utils.enums import Tags
 
@@ -11,5 +12,13 @@ router = APIRouter(prefix="/products", tags=[Tags.products])
 async def read_single_product(product_id: str):
     try:
         return await read_product_by_id(product_id)
+    except HTTPException as e:
+        raise e
+
+
+@router.post("", response_model=ProductResponse, status_code=201)
+async def create_single_product(product: ProductRequest):
+    try:
+        return await create_product(product)
     except HTTPException as e:
         raise e
