f85faa2f2a9b4627204a4ef62f1762b3bc2c65c3
rifatrakib <Rakib.1508@outlook.com>
2023-08-15 17:23:51 +0600

    added new base schema for shop

--- server/schemas/common/products.py
+++ server/schemas/common/products.py
@@ -1,6 +1,6 @@
-from typing import List, Union
+from typing import Dict, List, Union
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, EmailStr, Field, HttpUrl
 
 
 class ProductBase(BaseModel):
@@ -12,3 +12,11 @@ class ProductBase(BaseModel):
     available_sizes: List[str] = Field(default_factory=list, title="Available sizes of the product")
     colors: List[str] = Field(default_factory=list, title="Available colors of the product")
     rating: Union[float, None] = Field(default=None, title="Average product rating")
+
+
+class ShopBase(BaseModel):
+    name: str = Field(..., title="Shop name")
+    address: str = Field(..., title="Shop address")
+    phone_numbers: Union[List[str], None] = Field(default=None, title="Shop phone numbers")
+    emails: Union[List[EmailStr], None] = Field(default=None, title="Shop emails")
+    links: Union[Dict[str, HttpUrl], None] = Field(default=None, title="Shop links")
